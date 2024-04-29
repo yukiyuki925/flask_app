33b37859e996c067cf4f1b6dadcb7d735712eb7d
yukiyuki925 <yuki.work1999@gmail.com>
2024-04-30 08:08:28 +0900

    crud機能作成

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template,g
+from flask import render_template,g,redirect,request
 import sqlite3
 DATABASE="flaskmemo.db"
 
@@ -9,6 +9,33 @@ app = Flask(__name__)
 def top():
   memo_list = get_db().execute("select id,title,body from memo").fetchall()
   return render_template('index.html', memo_list=memo_list)
+
+@app.route("/regist", methods=['GET','POST'])
+def regist():
+  if request.method == 'POST':
+    # 画面から登録情報の取得
+    title = request.form.get('title')
+    body = request.form.get('body')
+    db = get_db()
+    db.execute("insert into memo (title,body) values(?,?)",[title,body])
+    db.commit()
+    return redirect('/')
+  return render_template('regist.html')
+
+@app.route("/<id>/edit", methods=['GET','POST'])
+def edit(id):
+
+  if request.method == 'POST':
+    # 画面から登録情報の取得
+    title = request.form.get('title')
+    body = request.form.get('body')
+    db = get_db()
+    db.execute("update memo set title=?, body=? where id=?",[title,body,id])
+    db.commit()
+    return redirect('/')
+  
+  post = get_db().execute("select id,title,body from memo where id=?" ,(id,)).fetchone()
+  return render_template('edit.html', post=post)
 
 if __name__ == "__main__":
     app.run()
