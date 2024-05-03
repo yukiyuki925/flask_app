d21925df32c415714d6198f3bc59b621564761b2
yukiyuki925 <yuki.work1999@gmail.com>
2024-05-03 13:48:35 +0900

    login complete

--- app.py
+++ app.py
@@ -2,26 +2,61 @@ from flask import Flask
 from flask import render_template,g,redirect,request
 import sqlite3
 DATABASE="flaskmemo.db"
-from flask_login import UserMixin, LoginManager
+from flask_login import UserMixin, LoginManager,login_required,login_user, logout_user
+
 
 app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
+app.secret_key = 'taro'
 
 class User(UserMixin):
   def __init__(self,userid):
     self.id = userid
 
+# ログイン
 @login_manager.user_loader
 def load_user(userid):
   return User(userid)
+@login_manager.unauthorized_handler
+def unauthorized():
+  return redirect('/login')
+
+# ログアウト
+@app.route("/logout",methods=["GET"])
+def logout():
+  logout_user()
+  return redirect("/login")
+
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+  error_message = ''
+  userid = ''
+
+  if request.method == 'POST':
+    userid = request.form.get('userid')
+    password = request.form.get('password')
+    # ログインのチェック
+    if(userid == 'taro' and password == '1234'):
+      user = User(userid)
+      login_user(user)
+      return redirect('/')
+    else:
+      error_message = "入力されたIDまたはパスワードが不正です。"
+
+  return render_template('login.html', userid=userid, error_message=error_message)
 
 @app.route("/")
+@login_required
 def top():
+  # memo_listの中にデータベースの値を全てぶちこむ
   memo_list = get_db().execute("select id,title,body from memo").fetchall()
+  # index.htmlにmemo_listの値を渡す
   return render_template('index.html', memo_list=memo_list)
 
 @app.route("/regist", methods=['GET','POST'])
+@login_required
 def regist():
   if request.method == 'POST':
     # 画面から登録情報の取得
@@ -34,6 +69,7 @@ def regist():
   return render_template('regist.html')
 
 @app.route("/<id>/edit", methods=['GET','POST'])
+@login_required
 def edit(id):
   if request.method == 'POST':
     # 画面から登録情報の取得
@@ -48,6 +84,7 @@ def edit(id):
   return render_template('edit.html', post=post)
   
 @app.route("/<id>/delete",methods=['GET','POST'])
+@login_required
 def delete(id):
   if request.method == 'POST':
     # 画面から登録情報の取得
@@ -60,7 +97,7 @@ def delete(id):
   return render_template('delete.html', post=post)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run()
 
 def connect_db():
   rv = sqlite3.connect(DATABASE)
